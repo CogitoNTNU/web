eca905ee954de660ad08af8f1424252e5c46b744
hellemg <helle.graaberg@hotmail.com>
2018-02-13 18:46:26 +0100

    Import semantic and start developing mainpage

--- web/urls.py
+++ web/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 
 from web.views import IndexView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', IndexView.as_view()),
+    re_path('^$', IndexView.as_view()),
 ]
