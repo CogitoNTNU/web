2d0e094752c1eb73b812fefdc3dc7a65d7156d84
Amund Tenstad <github@amund.io>
2018-05-06 11:00:44 +0200

    Implement concurrent update

--- concurrency/models.py
+++ concurrency/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class ConcurrentModel(models.Model):
+    concurrency_user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True, )
+    concurrency_key = models.CharField(max_length=32, blank=True, null=True)
+    concurrency_time = models.DateTimeField(blank=True, null=True)
+
+    def concurrency_reset(self):
+        self.concurrency_key = None
+        self.concurrency_user = None
+        self.concurrency_time = None
+        self.save()
