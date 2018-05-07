9d5249052bd737b83f06b28b4180fb9631cca261
Amund Tenstad <github@amund.io>
2018-05-07 13:28:25 +0200

    Use ckeditor

--- news/models.py
+++ news/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 from concurrency.models import ConcurrentModel
 
 
 class Article(ConcurrentModel):
     title = models.CharField(max_length=100)
-    content = models.TextField(blank=True, null=True)
+    content = RichTextField(blank=True, null=True)
 
     def __str__(self):
         return self.title
