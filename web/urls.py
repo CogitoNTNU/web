43bf700a1bc0b4096b5364048c9d4ac13a4a91ed
Amund Tenstad <github@amund.io>
2018-05-09 09:13:14 +0200

    Fix complete path

--- web/urls.py
+++ web/urls.py
@@ -9,11 +9,11 @@ from web.views import Home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('complete/<str:backend>/', login_wrapper),
     path('', include('social_django.urls', namespace='social')),
     path('', Home.as_view(), name='home'),
     path('login/', RedirectView.as_view(url='/login/dataporten/'), name='login'),
     path('logout/', Logout.as_view(), name='logout'),
-    re_path(r'^complete/(?P<backend>[^/]+){0}/$', login_wrapper),
     path('news/', include('news.urls')),
     path('contentbox/', include('contentbox.urls')),
     path('committees/', CommitteeList.as_view(), name='committees'),
