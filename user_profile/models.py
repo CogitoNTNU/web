06330618d783c7ba4ee1f6ff42a80928d130f4b4
jonatan <jonaengs@gmail.com>
2018-07-07 23:51:05 +0200

    added app, basic models for user_profile

---
+++ user_profile/models.py
@@ -0,0 +1,33 @@
+from ckeditor_uploader.fields import RichTextUploadingField
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Profile(models.Model):
+    projects = models.ManyToManyField('user_profile.Project', related_name='users')
+    skills = models.ManyToManyField('user_profile.Skill', related_name='users')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Project(models.Model):
+    title = models.CharField(
+        blank=False,
+        max_length=140,
+    )
+    description = RichTextUploadingField(
+        blank=True,
+        max_length=5000
+    )
+    members = models.ManyToManyField('user_profile.Profile', related_name='projects')
+
+
+class Skill(models.Model):
+    name = models.CharField(
+        unique=True,
+        blank=False,
+        max_length=140,
+    )
+    users = models.ManyToManyField('user_profile.Profile', related_name='skills')
