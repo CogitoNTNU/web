f02ec87fec5a322230c204485e4eae2385214c9d
jonatan <jonaengs@gmail.com>
2018-07-09 22:17:21 +0200

    simplified profile page to a single view and url

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import url
 
-from .views import ProfileDetailView, specific_profile, self_profile
+from .views import profile
 
 urlpatterns = [
-    url(r'(?P<username>[-\w.]+)/$', specific_profile, name='profile'),
+    url(r'(?P<username>[-\w.]+)/$', profile, name='profile'),
     #url(r'info/skills/$')
-    #url(r'profile/(?P<username>[-\w.]+)/$', profile, name='profile'),
-]
+    ]
--- user_profile/views.py
+++ user_profile/views.py
@@ -5,30 +5,14 @@ from django.views.generic import DetailView
 from .models import Profile
 
 
-class ProfileDetailView(DetailView):
-    model = Profile
-
-    def get_object(self):
-        return Profile.objects.get(user=self.request.user)
-
-
 ##############################################
 
-def specific_profile(request, username):
+def profile(request, username):
     user = get_object_or_404(User, username=username)
-    return get_profile(request, user)
-
-
-def self_profile(request):
-    user = get_object_or_404(User, user=request.user)
-    return get_profile(request, user)
-
-
-def get_profile(request, user):
     try:
         profile = user.profile
-        return render(request, 'user_profile/profile.html', {'profile': profile})
+        return render(request, 'user_profile/profile.html', {'profile': profile, 'edit': request.user == user})
     except Profile.DoesNotExist:
         Profile(user=user)
-        return render(request, 'user_profile/profile.html', {'profile': user.profile})
+        return render(request, 'user_profile/profile.html', {'profile': user.profile, 'edit': request.user == user})
 
