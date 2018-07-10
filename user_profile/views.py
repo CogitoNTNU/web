dac4a89cb470adf674c6091436f5a69a578b83a5
jonatan <jonaengs@gmail.com>
2018-07-11 00:08:37 +0200

    First try at adding a form to the profile page

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
 from .models import Profile
-
+from .forms import ProfileForm
 
 ##############################################
+
 
 def profile(request, username):
     user = get_object_or_404(User, username=username)
@@ -12,5 +15,17 @@ def profile(request, username):
         user.profile  # Accessing a non-existent profile (they do no exist by default) triggers an error
     except Profile.DoesNotExist:
         Profile(user=user)
-    return render(request, 'user_profile/profile.html', {'profile': user.profile, 'edit': request.user == user})
+
+    form = ProfileForm()
+    if request.method == 'POST':
+        form = ProfileForm(request.POST)
+        if form.is_valid():
+            user.profile.skills = form.cleaned_data['skills']
+            # return HttpResponseRedirect(reverse('user_profile:profile', kwargs={'username': username}))
+
+    return render(
+        request,
+        'user_profile/profile.html',
+        {'profile': user.profile, 'edit': request.user == user, 'form': form}
+    )
 
