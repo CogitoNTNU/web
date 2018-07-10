ab95144f3da01a6f44f4994d03e9b942b7c313b4
jonatan <jonaengs@gmail.com>
2018-07-10 18:29:28 +0200

    started working on a model for editing profile info

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
 
 from .models import Profile
 
@@ -10,9 +9,8 @@ from .models import Profile
 def profile(request, username):
     user = get_object_or_404(User, username=username)
     try:
-        profile = user.profile
-        return render(request, 'user_profile/profile.html', {'profile': profile, 'edit': request.user == user})
+        user.profile  # Accessing a non-existent profile (they do no exist by default) triggers an error
     except Profile.DoesNotExist:
         Profile(user=user)
-        return render(request, 'user_profile/profile.html', {'profile': user.profile, 'edit': request.user == user})
+    return render(request, 'user_profile/profile.html', {'profile': user.profile, 'edit': request.user == user})
 
