38cb3f67e6cbd154c61ba1f00a9814e4107595e4
jonatan <jonaengs@gmail.com>
2018-07-11 23:59:25 +0200

    fixed copy-pasted coded in profile.html

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -4,6 +4,6 @@ from .views import profile, DetailSkillView
 
 urlpatterns = [
     url(r'(?P<username>[-\w.]+)/$', profile, name='profile'),
-    url(r'skill/(?P<pk>\d+)/$', DetailSkillView.as_view(), name='skill_detail'),
+    url(r'skill/(?P<pk>\d+)$', DetailSkillView.as_view(), name='skill_detail'),
     ]
 
