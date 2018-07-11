8929d5e5ae8f27eaf67000532cd61dc4c908e930
jonatan <jonaengs@gmail.com>
2018-07-11 23:28:22 +0200

    added a simple function for populating my database in case i need to flush again

--- user_profile/views.py
+++ user_profile/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views.generic import DetailView
 
-from .models import Profile, Skill
+from .models import Profile, Skill, Project
 from .forms import ProfileForm
 
 
@@ -35,4 +35,18 @@ def profile(request, username):
         'user_profile/profile.html',
         {'profile': user.profile, 'edit': request.user == user, 'form': form}
     )
+
+
+def populate():
+    s1 = Skill(name='ku')
+    s2 = Skill(name='hest')
+    s3 = Skill(name='gris')
+
+    for s in [s1, s2, s3]:
+        s.save()
+
+    p1 = Project(title='Nytt hus')
+    p2 = Project(title='Male båt')
+    p1.save()
+    p2.save()
 
