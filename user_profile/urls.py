c651c83dfdde572f8e7cc5412401fd191e6b4a5f
jonatan <jonaengs@gmail.com>
2018-07-14 01:36:34 +0200

    tried github fixed for picture, though they dont seem to work

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from .views import profile, DetailSkillView, DetailProjectView, CreateProjectView, apply_to_project, DeleteProjectView, \
     EditProjectView, ListProjectView
@@ -16,5 +17,9 @@ urlpatterns = [
 
     # must always be at the end, else it will interpret skill, project etc. as usernames
     url(r'(?P<username>[-\w.]+)/$', profile, name='profile'),
-    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
