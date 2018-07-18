3c5f53ead5c99493f1c879bdb51ecd13238b5c6f
jonatan <jonaengs@gmail.com>
2018-07-18 12:26:03 +0200

    recommendation/urls.py now uses path instead of url

--- recommendation/urls.py
+++ recommendation/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
+from django.urls import path
 from .views import CreateEntryView, DeleteEntryView, \
     EditEntryView, view_entry, CreateTagView, ListEntriesView
 
 urlpatterns = [
-    url(r'^$', ListEntriesView.as_view(), name="entry_list"),
-    url(r'^create/$', CreateEntryView.as_view(), name="entry_form"),
-    url(r'^edit/(?P<pk>\d+)/$', EditEntryView.as_view(), name="edit_entry"),
-    url(r'^delete/(?P<pk>\d+)/$', DeleteEntryView.as_view(), name="delete_entry"),
-    url(r'^detail/(?P<pk>\d+)/$', view_entry, name="entry_detail"),
-    url(r'^tag/add/$', CreateTagView.as_view(), name="tag_form"),
+    path('create/', CreateEntryView.as_view(), name="entry_form"),
+    path('edit/<int:pk>/', EditEntryView.as_view(), name="edit_entry"),
+    path('delete/<int:pk>/', DeleteEntryView.as_view(), name="delete_entry"),
+    path('detail/<int:pk>/', view_entry, name="entry_detail"),
+    path('tag/add/', CreateTagView.as_view(), name="tag_form"),
+    path('/', ListEntriesView.as_view(), name="entry_list"),
 ]
