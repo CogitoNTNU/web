72ce3ca877acc700a4416fe57fb82c52854a48ee
jonatan <jonaengs@gmail.com>
2018-07-18 11:58:51 +0200

    removed print statement, removed unrelated_entries param (and related code) from get_related_entries helper

--- recommendation/helpers.py
+++ recommendation/helpers.py
@@ -9,7 +9,7 @@ def get_entry_tags(entry):
     return entry.tags[1:-1].replace("'", "").split(",")
 
 
-def get_related_entries(entry, unrelated_entries=True):
+def get_related_entries(entry):
     """
     :param entry: Entry object
     :param unrelated_entries: boolean -> Return entries with zero tags in common with param entry
@@ -21,30 +21,18 @@ def get_related_entries(entry, unrelated_entries=True):
     entry_tag_dict.pop(entry)  # removes param entry from list of entries to avoid matching with itself
     for key, value in entry_tag_dict.items():  # switches the values from lists of tags to # tags in common with param
         entry_tag_dict[key] = len(set(tags).intersection(set(value)))
-    print(entry_tag_dict)
 
     # creates the list mentioned as :return
     sorted_entry_list = []  # The starting list must contain at least one item
-    if unrelated_entries:
-        for key, value in entry_tag_dict.items():
-            i = 0
-            try:
-                while value < entry_tag_dict[sorted_entry_list[i]]:
-                    i += 1
-            except IndexError:
-                pass
-            finally:
-                sorted_entry_list.insert(i, key)
-    else:  # Two separate loops is more efficient than checking unrelated_entries every time in a single loop
-        for key, value in entry_tag_dict.items():
-            i = 0
-            try:
-                while value < entry_tag_dict[sorted_entry_list[i]]:
-                    i += 1
-            except IndexError:
-                pass
-            finally:
-                sorted_entry_list.insert(i, key)
+    for key, value in entry_tag_dict.items():
+        i = 0
+        try:
+            while value < entry_tag_dict[sorted_entry_list[i]]:
+                i += 1
+        except IndexError:
+            pass
+        finally:
+            sorted_entry_list.insert(i, key)
 
     return sorted_entry_list
 
