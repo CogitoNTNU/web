a6ae594d82056f4a52255f8d5386b0232494c0dd
jonatan <jonaengs@gmail.com>
2018-07-18 12:36:53 +0200

    user_profile/urls.py now uses path instead of url. Small fix in profile template

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
@@ -7,21 +8,21 @@ from .views import profile, DetailSkillView, DetailProjectView, CreateProjectVie
     EditProjectView, ListProjectView, administrate_project, accept_applicant, reject_applicant
 
 urlpatterns = [
-    url(r'projects/$', ListProjectView.as_view(), name='project_list'),
-    url(r'project/(?P<pk>\d+)/admin/$', administrate_project, name='project_admin'),
+    path('projects/', ListProjectView.as_view(), name='project_list'),
+    path('project/<int:pk>/admin/', administrate_project, name='project_admin'),
 
-    url(r'project/(?P<pk>\d+)/accept/(?P<username>[-\w.]+)/$', accept_applicant, name='accept_applicant'),
-    url(r'project/(?P<pk>\d+)/reject/(?P<username>[-\w.]+)/$', reject_applicant, name='reject_applicant'),
+    path('project/(<int:pk>/accept/<username>/', accept_applicant, name='accept_applicant'),
+    path('project/<int:pk>/reject/<username>/', reject_applicant, name='reject_applicant'),
 
-    url(r'project/(?P<pk>\d+)/delete/$', DeleteProjectView.as_view(), name='delete_project'),
-    url(r'project/(?P<pk>\d+)/edit/$', EditProjectView.as_view(), name='edit_project'),
-    url(r'project/(?P<pk>\d+)/apply/$', apply_to_project, name='apply_to_project'),
-    url(r'project/(?P<pk>\d+)$', DetailProjectView.as_view(), name='project'),
-    url(r'project/new/$', CreateProjectView.as_view(), name='project_form'),
-    url(r'skill/(?P<pk>\d+)$', DetailSkillView.as_view(), name='skill'),
+    path('project/<int:pk>/delete/', DeleteProjectView.as_view(), name='delete_project'),
+    path('project/<int:pk>/edit/', EditProjectView.as_view(), name='edit_project'),
+    path('project/<int:pk>/apply/', apply_to_project, name='apply_to_project'),
+    path('project/<int:pk>/', DetailProjectView.as_view(), name='project'),
+    path('project/new/', CreateProjectView.as_view(), name='project_form'),
+    path('skill/<int:pk>/', DetailSkillView.as_view(), name='skill'),
 
     # must always be at the end, else it will interpret skill, project etc. as usernames
-    url(r'(?P<username>[-\w.]+)/$', profile, name='profile'),
+    path('<username>/', profile, name='profile'),
     ]
 
 urlpatterns += staticfiles_urlpatterns()
