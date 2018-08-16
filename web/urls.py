71e1182e06c0086720c52c5dec854747b71c7608
jonatan <jonaengs@gmail.com>
2018-08-16 16:06:24 +0200

    Resources (in the navbar) now links to resource_list

--- web/urls.py
+++ web/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path('contentbox/', include('contentbox.urls')),
     path('committees/', CommitteeList.as_view(), name='committees'),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-    path('resource/', include('resource.urls')),
+    path('resources/', include('resource.urls')),
     path('profiles/', include('user_profile.urls')),
     ContentBox.url('about'),
 ]
