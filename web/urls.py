6699854e83502aca8aad802fb7d8b681065c2fa3
jonatan <jonaengs@gmail.com>
2018-08-16 15:53:31 +0200

    removed unused import

--- web/urls.py
+++ web/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.views.generic import RedirectView
 
 from contentbox.models import ContentBox
