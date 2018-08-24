0f2a20c68ff603b416ef17222a6464542d420677
jonatan <jonaengs@gmail.com>
2018-08-24 13:00:56 +0200

    added success message when creating new tags

--- resource/views.py
+++ resource/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DeleteView, UpdateView
@@ -29,13 +30,19 @@ class EditResourceView(PermissionRequiredMixin, UpdateView):
     form_class = ResourceForm
 
 
-class CreateTagView(PermissionRequiredMixin, CreateView):
+class CreateTagView(PermissionRequiredMixin, SuccessMessageMixin, CreateView):
     permission_required = 'resource.add_tag'
     redirect_field_name = 'recommend/resource_list.html'
     form_class = TagForm
     model = Tag
     success_url = '/resources/tag/add/'
-    success_message = 'Tag added to database!'
+    success_message = '%(name)s was added successfully!'
+
+    def get_success_message(self, cleaned_data):
+        return self.success_message % dict(
+            cleaned_data,
+            name=self.object.name,
+        )
 
 
 ###########################################################
