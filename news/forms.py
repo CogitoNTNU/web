365d48dc75af4d091a57e313b479391132a20981
jonatan <jonaengs@gmail.com>
2018-08-30 16:07:19 +0200

    excluded concurrency fields from eventform

--- news/forms.py
+++ news/forms.py
@@ -6,7 +6,7 @@ class EventForm(forms.ModelForm):
 
     class Meta:
         model = Event
-        fields = '__all__'
+        exclude = ['concurrency_user', 'concurrency_key', 'concurrency_time']
 
         widgets = {
             'start_date': forms.DateInput(attrs={'type': 'date'}),
