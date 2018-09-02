df1e3ed1db5ecf7f8c633a0751ed33f51a2ba9ac
jonatan <jonaengs@gmail.com>
2018-09-02 15:41:39 +0200

    started work on getting both articles and events listed on the front page

--- web/views.py
+++ web/views.py
@@ -1,9 +1,14 @@
+from itertools import chain
+
 from django.utils import timezone
 from django.views.generic import ListView
 
-from news.models import Event
+from news.models import Event, Article
 
 
 class Home(ListView):
-    queryset = Event.objects.filter(start_date__gte=timezone.now(), published=True)
+    queryset_event = Event.objects.filter(start_date__gte=timezone.now(), published=True)
+    queryset_article = Article.objects.filter().exclude(id__in=Event.objects.all())
+    queryset = list(chain(queryset_event, queryset_article))
+
     template_name = 'web/index.html'
