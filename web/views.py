7fbef8f5d3bfae97d970ba12c1cafa8b33c93959
jonatan <jonaengs@gmail.com>
2018-09-28 20:35:53 +0200

    small fix

--- web/views.py
+++ web/views.py
@@ -1,5 +1,3 @@
-from itertools import chain
-
 from django.utils import timezone
 from django.views.generic import ListView
 
@@ -9,6 +7,6 @@ from news.models import Event, Article
 class Home(ListView):
     # queryset_event = Event.objects.filter(start_date__gte=timezone.now(), published=True)
     # queryset_article = Article.objects.filter().exclude(id__in=Event.objects.all())
-    queryset = Article.objects.filter(published=True).exclude(id__in=Event.objects.filter(start_date__gte=timezone.now()))
-
+    queryset = Article.objects.filter(published=True)\
+        .exclude(id__in=Event.objects.filter(start_date__lte=timezone.now()))
     template_name = 'web/index.html'
