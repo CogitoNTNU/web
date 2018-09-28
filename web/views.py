4d2a597fceebb0ada9a664e83d6f5388fd7181a3
jonatan <jonaengs@gmail.com>
2018-09-28 20:37:53 +0200

    small fix

--- web/views.py
+++ web/views.py
@@ -5,8 +5,6 @@ from news.models import Event, Article
 
 
 class Home(ListView):
-    # queryset_event = Event.objects.filter(start_date__gte=timezone.now(), published=True)
-    # queryset_article = Article.objects.filter().exclude(id__in=Event.objects.all())
     queryset = Article.objects.filter(published=True)\
         .exclude(id__in=Event.objects.filter(start_date__lte=timezone.now()))
     template_name = 'web/index.html'
