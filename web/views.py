1279df49e0c576cf027db02cbded90d8601a4a38
jonatan <jonaengs@gmail.com>
2018-09-30 14:18:53 +0200

    index page now displays both articles and events correctly

--- web/views.py
+++ web/views.py
@@ -2,10 +2,17 @@ from django.utils import timezone
 from django.views.generic import ListView
 
 from news.models import Event, Article
+from itertools import chain
+
 
 
 class Home(ListView):
+    """
     queryset = Article.objects.filter(published=True)\
         .exclude(id__in=Event.objects.filter(end_date__lt=timezone.now()))\
         .order_by('-datetime_published')
+    """
+    articles = Article.objects.filter(published=True).exclude(id__in=Event.objects.all())
+    events = Event.objects.filter(end_date__gt=timezone.now())
+    queryset = sorted(chain(articles, events), key=lambda o: o.datetime_published)
     template_name = 'web/index.html'
