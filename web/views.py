9fe469dc2c372fbe37c0ecc6768ca0e859cbb24b
jonatan <jonaengs@gmail.com>
2018-10-01 16:29:03 +0200

    changed index qs

--- web/views.py
+++ web/views.py
@@ -12,7 +12,7 @@ class Home(ListView):
         .exclude(id__in=Event.objects.filter(end_date__lt=timezone.now()))\
         .order_by('-datetime_published')
     """
-    articles = Article.objects.filter(published=True).exclude(id__in=Event.objects.all())
-    events = Event.objects.filter(end_date__gt=timezone.now())
-    queryset = sorted(chain(articles, events), key=lambda o: o.datetime_published)
+    queryset = Article.objects.filter(published=True).exclude(id__in=Event.objects.all())
+    #events = Event.objects.filter(end_date__gt=timezone.now())
+    #queryset = sorted(chain(articles, events), key=lambda o: o.datetime_published)
     template_name = 'web/index.html'
