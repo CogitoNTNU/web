ff5c26d2cb9e2323a8401978a6f7c89d06024d8c
jonatan <jonaengs@gmail.com>
2018-11-05 18:36:30 +0100

    reduced profile view, tried creating class-based version

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from django.views.generic import DetailView
 from .models import Skill
 
-from .views import profile
+from .views import profile, ProfileView
 
 urlpatterns = [
     path('skill/<int:pk>/', DetailView.as_view(model=Skill), name='skill'),
 
     # must always be at the end, else it will interpret skill, project etc. as usernames
-    path('<username>/', profile, name='profile'),
+    path('<str:username>/', profile, name='profile'),
     ]
--- user_profile/views.py
+++ user_profile/views.py
@@ -2,8 +2,9 @@ from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.views.generic import DetailView
 
-from .models import Profile, Skill
+from .models import Profile
 from .forms import ProfileForm
 
 
@@ -14,27 +15,54 @@ def profile(request, username):
     except Profile.DoesNotExist:
         Profile.objects.create(user=user)
 
-    form = ProfileForm()
     if request.method == 'POST':
         form = ProfileForm(request.POST, request.FILES)
         if form.is_valid() and request.user == user:
-            # removing the connection one way does not remove it the other way...?
-            user.profile.skills.clear()
-            for skill in Skill.objects.all():
-                skill.members.remove(user.profile)
+            skills = form.cleaned_data['skills']
+            user.profile.skills.set(skills)
             for skill in form.cleaned_data['skills']:
-                user.profile.skills.add(skill)
                 skill.members.add(user.profile)
-            """
-            user.profile.picture.delete()
-            user.profile.picture = form.cleaned_data['picture']
-            user.profile.save()
-            """
+
             return HttpResponseRedirect(reverse('profile', kwargs={'username': username}))
 
-    return render(
-        request,
-        'user_profile/profile.html',
-        {'profile': user.profile, 'edit': request.user == user, 'form': form}
-    )
+    return render(request, 'user_profile/profile.html', {'profile': user.profile, 'form': ProfileForm()})
+
+
+"""
+class ProfileView(DetailView):
+    model = Profile
+    pk_url_kwarg = 'username'
+    template_name = 'user_profile/profile.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        user = get_object_or_404(User, username=kwargs['username'])
+        # Accessing a non-existent profile (they do no exist by default) triggers an error
+        try:
+            user.profile
+        except Profile.DoesNotExist:
+            Profile.objects.create(user=user)
+        if request.method.lower() in self.http_method_names:
+            handler = getattr(self, request.method.lower(), self.http_method_not_allowed)
+        else:
+            handler = self.http_method_not_allowed
+        return handler(request, *args, **kwargs)
+
+    def get_object(self, queryset=None):
+        return User.objects.get(username=self.kwargs['username']).profile
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = ProfileForm()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = get_object_or_404(User, username=kwargs['username'])
+            if request.user == user:
+                user.profile.skills.set(form.cleaned_data['skills'])
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+
+"""
+
 
