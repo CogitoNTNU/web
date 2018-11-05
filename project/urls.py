939ad238ed60a7cf225991490a51c9f14f55a931
jonatan <jonaengs@gmail.com>
2018-11-05 19:43:52 +0100

    update urls.py to match new view

--- project/urls.py
+++ project/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from django.views.generic import ListView, DetailView
 
 from project.models import Project
-from project.views import administrate_project, accept_applicant, reject_applicant, DeleteProjectView, EditProjectView, \
+from project.views import ProjectAdminDetailView, accept_applicant, reject_applicant, DeleteProjectView, EditProjectView, \
     apply_to_project, CreateProjectView
 
 urlpatterns = [
     path('', ListView.as_view(model=Project), name='project_list'),
-    path('<int:pk>/admin/', administrate_project, name='project_admin'),
+    path('<int:pk>/admin/', ProjectAdminDetailView.as_view(), name='project_admin'),
 
     path('<int:pk>/accept/<username>/', accept_applicant, name='accept_applicant'),
     path('<int:pk>/reject/<username>/', reject_applicant, name='reject_applicant'),
