7aff9d6183a0f5a557e6bcc60d80bfb43d7dcf61
Jonatan S. Engstad <jonaengs@gmail.com>
2019-01-17 22:27:45 +0100

    fix single image delete url

--- single_page/urls.py
+++ single_page/urls.py
@@ -8,9 +8,9 @@ from single_page.views import SinglePageCreateView, SinglePageUpdateView, Single
 urlpatterns = [
     path('page/create', SinglePageCreateView.as_view(), name='create_single_page'),
     path('image/create', SingleImageCreateView.as_view(), name='create_single_image'),
+    path('i/<str:slug>/delete', SingleImageDeleteView.as_view(), name='delete_single_image'),
+    path('i/<str:slug>/', image_view, name='single_image'),
     path('<str:slug>/update', SinglePageUpdateView.as_view(), name='change_single_page'),
     path('<str:slug>/delete', SinglePageDeleteView.as_view(), name='delete_single_page'),
-    path('<str:slug>/delete', SingleImageDeleteView.as_view(), name='delete_single_image'),
     path('<str:slug>/', DetailView.as_view(model=SinglePage), name='single_page'),
-    path('i/<str:slug>/', image_view, name='single_image'),
 ]
