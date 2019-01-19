cda517a54489174fbfb0c29ed38259f4c8655248
Jonatan S. Engstad <jonaengs@gmail.com>
2019-01-19 18:30:58 +0100

    allow passed events on front page

--- web/views.py
+++ web/views.py
@@ -11,5 +11,6 @@ class Home(ListView):
 
     def get_queryset(self):
         articles = Article.objects.filter(published=True).exclude(id__in=Event.objects.all())
-        events = Event.objects.filter(end_date__gt=timezone.now())
+        #events = Event.objects.filter(end_date__gt=timezone.now())
+        events = Event.objects.all()
         return list(reversed(sorted(chain(articles, events), key=lambda o: o.datetime_created)))
