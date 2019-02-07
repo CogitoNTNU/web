fb3528f2c5fef60b0bd063aa744aeb1a07915693
Sander <sander.k.kilen@gmail.com>
2019-02-07 17:36:13 +0100

    Add view and url for new skill_list.html page

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from django.views.generic import DetailView
 from .models import Skill
+from .views import SkillListView
 
 from .views import profile
 
 urlpatterns = [
+
+    path('skills/', SkillListView.as_view(), name='skill_list'),
+
     path('skill/<int:pk>/', DetailView.as_view(model=Skill), name='skill'),
 
     # must always be at the end, else it will interpret skill, project etc. as usernames
