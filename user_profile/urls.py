b39885358546a65b2bef748b1cc432dde451eb71
Sander <sander.k.kilen@gmail.com>
2019-03-14 17:27:36 +0100

    Demand user loged inn for viewing skill pages

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.views.generic import DetailView
+from django.contrib.auth.decorators import login_required
 from .models import Skill
 from .views import SkillListView
 
@@ -7,9 +8,9 @@ from .views import profile
 
 urlpatterns = [
 
-    path('skills/', SkillListView.as_view(), name='skill_list'),
+    path('skills/', login_required(SkillListView.as_view()), name='skill_list'),
 
-    path('skill/<int:pk>/', DetailView.as_view(model=Skill), name='skill'),
+    path('skill/<int:pk>/', login_required(DetailView.as_view(model=Skill)), name='skill'),
 
     # must always be at the end, else it will interpret skill, project etc. as usernames
     path('<str:username>/', profile, name='profile'),
